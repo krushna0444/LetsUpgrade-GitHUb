1f8357f840369e7893796177a82f33ade2ffe06a
krushna0444 <krushnasa04@gmail.com>
2025-11-05 22:37:47 +0530

    first commit

---
+++ Pcode/Calculater.py
@@ -0,0 +1,23 @@
+a=int(input("Enter the first number :"))
+op=input("Enter the opertion to perfrom  ('+','-','*','/')")
+b=int(input('"Enter the second number :'))
+
+if op=='+':
+    result= a+b
+    print("addition :-",result)
+elif op=='-':
+    result= a-b
+    print("Subtration :-",result)
+elif op=='*':
+    result= a*b
+    print("mulitipilcation :-",result)
+elif op=='/':
+    if b!=0:
+       result= a/b
+       print("Divisoon :-",result) 
+    else:
+        print("Zero is not divisibly")
+else:
+    print("Invaild choice")
+
+ 
